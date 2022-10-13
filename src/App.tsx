4c222df87c487f3361230e2d0ad7b523426a9794
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-13 11:49:36 -0400

    Added header text directly above the top most text

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1>Mock React Website Deployment Header</h1>
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
