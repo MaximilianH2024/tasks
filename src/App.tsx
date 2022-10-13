5991dda4eee26e6fca697fc9330cfed70cc095bc
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-13 17:18:27 -0400

    Final commit for task 2: All tests have been passed for checking for: Image, Minimum of 3 bullet points within a list, added a button that can log to console, added two columns directly above the image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <h1>Mock React Website Deployment Header</h1>
+            <h1 className="MockHeader">Mock React Website Deployment Header</h1>
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
-            </header>
-            <header>
                 <Button
                     className="Button"
                     onClick={() => console.log("Hello World!")}
@@ -17,10 +15,18 @@ function App(): JSX.Element {
                     Log Hello World
                 </Button>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload (Maximilian).
+            <p className="SimpleTextClass">
+                Edit <code>src/App.tsx</code> and save.
             </p>
+            <p className="SimpleTextClass">
+                This page will automatically reload (Maximilian).
+            </p>
+            <Container>
+                <Row className="ColumnTextClass">
+                    <Col>First Column</Col>
+                    <Col>Second Column</Col>
+                </Row>
+            </Container>
             <img
                 className="DeerPhoto"
                 src="https://image.shutterstock.com/image-photo/silhouette-large-bull-elk-stag-260nw-325904582.jpg"
