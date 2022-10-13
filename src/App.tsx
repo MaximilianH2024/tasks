6686d17d52bdaf111f9d6aa4174896ad3a8073ce
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-13 11:22:46 -0400

    Altered font style of main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 Hello World! UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
+            <p className="App-text">
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload COS420 (Maximilian).
             </p>
