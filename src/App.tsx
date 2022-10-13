d02766a8d844088ca142c289900f2abdc956aaf5
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-13 15:37:03 -0400

    Added a button with the text of Log Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,6 +8,9 @@ function App(): JSX.Element {
             <h1>Mock React Website Deployment Header</h1>
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
+            </header>
+            <header>
+                <Button className="Button">Log Hello World</Button>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
