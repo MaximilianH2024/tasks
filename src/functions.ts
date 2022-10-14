24c17c1d3c08926d27ff1cda08ed49f4077e3dc8
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-14 15:49:53 -0400

    Corectly deployed add 3 numbers function, as well as the to uppercase function

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,19 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+
+    return sum;
 }
 
 /**
@@ -20,7 +32,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    // const res = message.toUpperCase;
+
+    return message.toUpperCase() + "!";
 }
 
 /**
