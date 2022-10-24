3cf270afbd9a1a5b483981d94fdd439d6a82fb2b
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-24 11:33:58 -0400

    Implemented the change type component through defining an array of available question types, defining a state for use within the files html under the type QuestionType, and implementing a Button component through the combination of all of the previously mentioned elements

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,48 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    // Define a list of available question types, with the imported type of "QuestionType"
+    const qs: QuestionType[] = [
+        "short_answer_question",
+        "multiple_choice_question"
+    ];
+
+    // type questionType = "short_answer_question" | "short_answer_question"/
+
+    // eslint-disable-next-line prettier/prettier
+    // Set our initial type below to short_answer_question
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    return (
+        <div>
+            {/*
+            When our button is clicked, change its type to
+            multiple_choice_question if it is already short_answer_question,
+            otherwise change back to short_answer_question
+            */}
+            <Button
+                onClick={() => {
+                    setQuestionType(
+                        questionType === "short_answer_question"
+                            ? "multiple_choice_question"
+                            : "short_answer_question"
+                    );
+                }}
+            >
+                Change Type
+            </Button>
+            {/*
+            When the question type is short_answer_question, 
+            reveal the text, Short Answer, 
+            otherwise reveal the text Multiple Choice
+            */}
+            <div>
+                {questionType === "short_answer_question"
+                    ? "Short Answer"
+                    : "Multiple Choice"}
+            </div>
+        </div>
+    );
 }
