5084224a04c76af833fc31842aac3be9ace3383f
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-24 10:42:40 -0400

    Successfully implemented the reveal answer compnent

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        // set visible to be the logical operator of its previous value:
+        setVisible(!visible);
+    }
+
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {!visible && <div>42</div>}
+        </div>
+    );
 }
