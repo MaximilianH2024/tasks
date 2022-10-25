0b3a48fa698d70dbc6b9623650e832b433f857d0
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-10-24 23:00:50 -0400

    Added the Cycle Holiday component with two buttons

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,55 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const Holiday: string[] = ["🎁", "🐲", "🐇", "🎃", "🦃"];
+
+    const [holiday, changeHoliday] = useState<string>(Holiday[0]);
+
+    function holidayByChrono(holiday: string): string {
+        if (holiday === "🐲") {
+            return "🐇";
+        } else if (holiday === "🐇") {
+            return "🎃";
+        } else if (holiday === "🎃") {
+            return "🦃";
+        } else if (holiday === "🦃") {
+            return "🎁";
+        } else {
+            return "🐲";
+        }
+    }
+
+    function holidayByAlphabet(holiday: string): string {
+        if (holiday === "🐲") {
+            return "🎁";
+        } else if (holiday === "🎁") {
+            return "🐇";
+        } else if (holiday === "🐇") {
+            return "🎃";
+        } else if (holiday === "🎃") {
+            return "🦃";
+        } else {
+            return "🐲";
+        }
+    }
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    changeHoliday(holidayByAlphabet(holiday));
+                }}
+            >
+                Advance Holiday By Alphabet Order
+            </Button>
+            <div>{"Holiday: " + holiday}</div>
+            <Button
+                onClick={() => {
+                    changeHoliday(holidayByChrono(holiday));
+                }}
+            >
+                Advance Holiday By Year Order
+            </Button>
+        </div>
+    );
 }
