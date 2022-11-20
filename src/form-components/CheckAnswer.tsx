c85b616fef1570ff3bf89a13319435c678434922
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-11-20 15:34:39 -0500

    Until the answer is 42, a x is displayed, otherwise a check mark is shown

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Answer: </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={answer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>
+                <p>{answer === expectedAnswer ? "✔️" : "❌"}</p>
+            </div>
         </div>
     );
 }
