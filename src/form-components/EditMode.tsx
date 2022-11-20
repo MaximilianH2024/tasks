991135b34a2db5301d09c97f8a530622f6cb518d
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-11-20 17:57:27 -0500

    Added a switch to alternate between edit mode and disabled. Added a checkbox to either add is a student or is not a student to the users name at the bottom of the component.

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
+import { Form, FormControl } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    // By default, we will not be in edit mode to begin with:
+    const [editMode, setEditMode] = useState<boolean>(false);
+    // Users name is the string "Your Name"
+    const [name, setName] = useState<string>("Your Name");
+    // By default, the user will also be a student:
+    const [isAStudent, setStudent] = useState<boolean>(true);
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Group controlId="formResponse">
+                <Form.Label>Your Name: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={name}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setName(event.target.value)
+                    }
+                    disabled={!editMode}
+                />
+            </Form.Group>
+            {/* 
+                A switch that controls when one is in edit mode (initially false)
+            */}
+            <Form.Check
+                type="switch"
+                id="is-in-editmode-switch"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setEditMode(event.target.checked)
+                }
+            />
+            {/* 
+                Whether or not the user is a student should be controlled by a regular checkbox, not a switch or button 
+            */}
+            <Form.Check
+                type="checkbox"
+                id="is-student-check"
+                label="Student"
+                checked={isAStudent}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setStudent(event.target.checked)
+                }
+            />
+            {/* 
+                By default, will display "Your name is a student" or "Your name is not a student"
+            */}
+            <div>
+                {name} {isAStudent ? "is a student" : "is not a student"}
+            </div>
         </div>
     );
 }
