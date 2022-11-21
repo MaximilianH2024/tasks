a285677dcc894ff1f219bc6a78b2f05e10b1adc5
MaximilianHillgraf <maximilian.hillgraf@maine.edu>
2022-11-21 15:27:15 -0500

    Utilized the map function to minimize overall code over making one Forms.check tag for each color code

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("");
+
+    const colors = [
+        "Red",
+        "Olive",
+        "Yellow",
+        "Maroon",
+        "Blue",
+        "Aquamarine",
+        "Beige",
+        "Violet",
+        "Black"
+    ];
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((colorCode: string) => (
+                <Form.Check
+                    key={colorCode}
+                    label={colorCode}
+                    value={colorCode}
+                    type="radio"
+                    name="color"
+                    checked={colorCode === color}
+                    style={{ backgroundColor: colorCode }}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setColor(event.target.value)
+                    }
+                />
+            ))}
+            <h4 style={{ color: "Black" }}>
+                You have chosen{" "}
+                <div
+                    data-testid="colored-box"
+                    style={{ backgroundColor: color }}
+                >
+                    {" "}
+                    {color}{" "}
+                </div>
+            </h4>
         </div>
     );
 }
